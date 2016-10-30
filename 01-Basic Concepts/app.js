dbab7127a92bf3a5b250b550ea3fe1d47687ce42
Kowus <barnabas@elite-education.org>
2016-10-30 01:03:05 +0000

    Basic Concepts

---
+++ 01-Basic Concepts/app.js
@@ -0,0 +1,22 @@
+/*
+var person = {
+    firstName: "Barnabas",
+    lastName: "Nomo",
+    age: 28
+}
+console.log(person);
+*/
+
+/*
+function addNumber(a, b) {
+    return a + b;
+}
+console.log(addNumber(7, 3));
+*/
+
+var printBacon = function () {
+    console.log("Bacon is healthy, don't believe the doctors!");
+};
+// printBacon();
+
+setTimeout(printBacon, 5000);
