e9defcd993d665c0e560af464d435d45de84927a
Kowus <barnabas@elite-education.org>
2016-10-30 01:04:19 +0000

    03-Understanding Object References

---
+++ 03-Understanding Reference To Objects/app.js
@@ -0,0 +1,20 @@
+/*
+var Barnabas = {
+    favfood: 'bacon',
+    favMovie: 'Mr. Church'
+};
+
+
+var Person = Barnabas;
+Person.favfood = "salad";
+console.log(Barnabas.favfood);
+*/
+
+
+console.log(19 == '19');        // true
+console.log(19 === '19');       // false
+
+
+// Two equal signs are used when you want to compare values
+
+// Three equal signs are used when you want to compare values AND types
