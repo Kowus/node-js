932e88076da02dd0a886cbf4c9ab551cc9ce77b3
Kowus <barnabas@elite-education.org>
2016-10-30 01:06:58 +0000

    07-More On Modules

---
+++ 07-More On Modules/movies.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printAvatar: function () {
+        console.log("Avatar");
+    },
+    printChappie: function () {
+        console.log("Chappie");
+    },
+    favMovie: "The Matrix"
+};
+
+
