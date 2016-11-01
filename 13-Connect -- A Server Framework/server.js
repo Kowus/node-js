bed762e8d136ab088fd4b6809b5a3cb0d0aaeb35
Kowus <barnabas@elite-education.org>
2016-11-01 15:53:09 +0000

    13-Connect -- A Server Framework

--- 13-Connect -- A Server Framework/server.js
+++ 13-Connect -- A Server Framework/server.js
@@ -26,6 +26,16 @@ var http = require('http');
 
 var app = connect();
 
+function profile(request, response) {
+    console.log('User requested profile');
+}
+function forum(request, response) {
+    console.log('User requested forum');
+}
+
+app.use('/profile', profile);
+app.use('/forum', forum);
+
 
 http.createServer(app).listen(8888);
 console.log('Server is now running....................');
